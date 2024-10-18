63098870185fc033ba229852ceabb7b8f0be7f69
Michael Alexander <michael.lxndr@outlook.com>
2024-10-17 20:40:01 -0500

    tercer commit, trabajando con Git y GitHubClassroom

---
+++ proyecto02/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Ejemplo dos básico de Java");
+        System.out.println("Versionado a través de Git");
+        System.out.println("Manejado desde GitHub");
+        System.out.println("Además se usar GitHubClassroom");
+    }
+}
