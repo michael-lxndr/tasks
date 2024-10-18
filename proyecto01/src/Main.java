c969b3fb8f9f04d88e3bb2f8c64f16837244c843
Michael Alexander <michael.lxndr@outlook.com>
2024-10-17 20:27:08 -0500

    segundo commit

--- proyecto01/src/Main.java
+++ proyecto01/src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Ejemplo básico de Java");
-        System.out.println("Versionado a través de Git");
+        System.out.println("Ejemplo de Java");
         System.out.println("Manejado desde GitHub");
-        System.out.println("Recuerde usar la sentencia");
-        System.out.println("System.out.println(\"Ejemplo básico de Java Versionado a través de Git Manejado desde GitHub\");");
+        System.out.println("Versionado a través de Git");
+        System.out.println("Creado por <ubicar su usuario de github>");
     }
 }
