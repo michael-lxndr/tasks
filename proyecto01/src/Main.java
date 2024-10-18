b80a6e9cec2c32bcee1406baf3d39bec2e536cb6
Michael Alexander <michael.lxndr@outlook.com>
2024-10-17 20:17:54 -0500

    primer commit

---
+++ proyecto01/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Ejemplo básico de Java");
+        System.out.println("Versionado a través de Git");
+        System.out.println("Manejado desde GitHub");
+        System.out.println("Recuerde usar la sentencia");
+        System.out.println("System.out.println(\"Ejemplo básico de Java Versionado a través de Git Manejado desde GitHub\");");
+    }
+}
